065b4fbcaff148d312b42d9eb44620ab302f394f
Brant Fallin <brant.fallin@gmail.com>
2020-01-29 21:07:45 -0700

    Write viz:color alpha channel
    Fixes #3. Includes unit test to cover scenario.

---
+++ src/Gexf.UnitTests/VizColorPropertyTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing;
+using System.Xml.Linq;
+using FluentAssertions;
+using Gexf.Visualization.ExtensionProperties;
+using NUnit.Framework;
+
+namespace Gexf.UnitTests
+{
+    [TestFixture]
+    public class VizColorPropertyTests
+    {
+        [Test]
+        public void WriteToShouldOutputRgbWhenProvidedColorWithoutAlphaChannel()
+        {
+            Color expectedColor = Color.Coral;
+            var property = new VizColorProperty(expectedColor);
+
+            GexfOutput output =  new GexfOutput();
+            XElement element = new XElement("node");
+            property.WriteTo(output, element);
+
+            var viz = output.Viz.Namespace;
+            element.Should()
+                .HaveElement(viz + "color").Which.Should()
+                .HaveAttribute("r", $"{expectedColor.R}").And
+                .HaveAttribute("g", $"{expectedColor.G}").And
+                .HaveAttribute("b", $"{expectedColor.B}");
+
+            element.Attribute("a").Should().BeNull("the alpha channel was not specified");
+        }
+
+        [Test]
+        public void WriteToShouldOutputRgbaWhenProvidedColorWithAlphaChannel()
+        {
+            Color expectedColor = Color.FromArgb(20, Color.Coral);
+            var property = new VizColorProperty(expectedColor);
+
+            GexfOutput output = new GexfOutput();
+            XElement element = new XElement("node");
+            property.WriteTo(output, element);
+
+            var viz = output.Viz.Namespace;
+            element.Should()
+                .HaveElement(viz + "color").Which.Should()
+                .HaveAttribute("r", $"{expectedColor.R}").And
+                .HaveAttribute("g", $"{expectedColor.G}").And
+                .HaveAttribute("b", $"{expectedColor.B}").And
+                .HaveAttribute("a", "20");
+        }
+    }
+}
--- src/Gexf/Visualization/ExtensionProperties/VizColorProperty.cs
+++ src/Gexf/Visualization/ExtensionProperties/VizColorProperty.cs
@@ -5,6 +5,7 @@ namespace Gexf.Visualization.ExtensionProperties
 {
     public class VizColorProperty : IGexfExtensionProperty
     {
+        public const byte OpaqueAlphaChannel = 255;
         private readonly Color _color;
 
         public GexfId Id { get; } = "viz:color";
@@ -19,7 +20,10 @@ namespace Gexf.Visualization.ExtensionProperties
             element.Add(output.Viz.Element("color",
                 output.Attribute("r", _color.R),
                 output.Attribute("g", _color.G),
-                output.Attribute("b", _color.B)
+                output.Attribute("b", _color.B),
+
+                output.When(() => _color.A != OpaqueAlphaChannel, 
+                    () => output.Attribute("a", _color.A))
             ));
         }
     }
